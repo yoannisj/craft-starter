42d6bc92e2bd6942048f84fce093d5d25fbc76fe
Yoannis Jamar <office@yoannis.com>
2021-03-10 17:08:06 +0000

    add DATABASE_DRIVER environment variable
    - add DATABASE_DRIVER environment variable
    - use \craft\helpers\App::env() to configure the database connection using environment variables in config/db.php

--- craftcms/config/db.php
+++ craftcms/config/db.php
@@ -2,13 +2,15 @@
 
 // @see https://craftcms.com/docs/3.x/config/db-settings.html
 
+use craft\helpers\App;
+
 return [
-    'driver' => 'mysql',
-    'server' => getenv('DATABASE_SERVER'),
-    'port' => getenv('DATABASE_PORT'),
-    'user' => getenv('DATABASE_USER'),
-    'password' => getenv('DATABASE_PASSWORD'),
-    'database' => getenv('DATABASE_NAME'),
-    'schema' => getenv('DATABASE_SCHEMA'),
-    'tablePrefix' => getenv('DATABASE_TABLE_PREFIX'),
+    'driver' => App::env('DATABASE_DRIVER'),
+    'server' => App::env('DATABASE_SERVER'),
+    'port' => App::env('DATABASE_PORT'),
+    'user' => App::env('DATABASE_USER'),
+    'password' => App::env('DATABASE_PASSWORD'),
+    'database' => App::env('DATABASE_NAME'),
+    'schema' => App::env('DATABASE_SCHEMA'),
+    'tablePrefix' => App::env('DATABASE_TABLE_PREFIX'),
 ];
